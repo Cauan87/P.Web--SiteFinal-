db1ab6e87425095dbb609ee86fdc146f316b15b1
Pedro Cauan <p.cauan.sousa@gmail.com>
2023-06-27 00:24:04 +0000

    Arquivos reorganizados.

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import AddPost from './AddPost.jsx'
 import Feed from './Feed.jsx'
-import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
